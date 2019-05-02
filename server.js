79baf370e6ceda7408f8535bccc54292f209e721
Navin Kumar <navin1551@gmail.com>
2019-05-01 18:52:24 -0700

    api token testing

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ app.use(helmet());
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
   const authToken = req.get("Authorization");
-
+  console.log(apiToken);
+  console.log(authToken);
   if (!authToken || authToken.split(" ")[1] !== apiToken) {
     return res.status(401).json({ error: "Unauthorized request" });
   }
